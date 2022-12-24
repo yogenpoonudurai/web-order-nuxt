e75593a1da0a72335630ab93203470a261c965aa
Yogen <firacloudtech@gmail.com>
2022-12-25 01:21:59 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
