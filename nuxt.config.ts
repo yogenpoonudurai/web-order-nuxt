ec6d01765c0374ad4cd1585d173b46848891c50d
Yogen <firacloudtech@gmail.com>
2023-01-02 22:32:03 +0800

    added vercel deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: "firebase",
+    preset: "vercel", //firebase
     minify: true,
     compressPublicAssets: true,
   },
